f3591856d85ca3a371914b5fb2ffdcb3e31d8cdb
Qusay Mohammed Al-Amarat <qusayamarat@gmail.com>
2021-03-07 13:56:22 +0200

    create a new page

---
+++ app.js
@@ -0,0 +1,30 @@
+var animalName = prompt("What is your favourite animal Cat or Dog ?");
+
+while(!(animalName.toLowerCase()=="cat" || animalName.toLowerCase()=="dog"))
+{
+    animalName = prompt("What is your favourite animal Cat or Dog ?");
+}
+
+if(animalName.toLowerCase() == "cat")
+{
+    var image = "<img src='images/cat.jpg'>";
+}
+
+else
+{
+    var image = "<img src='images/dog.jpg'>";
+}
+
+document.write(image);
+
+var counter = 0;
+var imageSum = "";
+counter = prompt("How many times you want to show the animal image ?");
+
+while(counter > 0 )
+{
+    imageSum = imageSum + image;
+    counter -- ;
+}
+
+document.write(imageSum);
