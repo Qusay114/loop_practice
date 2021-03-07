21fb5ef00e931118a0805d7cf1308da0786240bd
Qusay Mohammed Al-Amarat <qusayamarat@gmail.com>
2021-03-07 14:35:11 +0200

    add a new img

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ while(!(animalName.toLowerCase()=="cat" || animalName.toLowerCase()=="dog"))
 if(animalName.toLowerCase() == "cat")
 {
     var image = "<img src='images/cat.jpg'>";
+    var image1 = "<img src='images/cat1.jpg'>";
 }
 else
 {
     var image = "<img src='images/dog.jpg'>";
+    var image1 = "<img src='images/dog1.jpg'>";
 }
 
-document.write(image);
+document.write(image1+'<br>');
 
 var counter = 0;
 var imageSum = "";
 counter = prompt("How many times you want to show the animal image ?");
 
-while(counter > 0 )
+while(counter > 1 )
 {
     imageSum = imageSum + image;
     counter -- ;
