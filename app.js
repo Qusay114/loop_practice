23f321f23c842c80a293667efd6f782e62f175ad
Qusay Mohammed Al-Amarat <qusayamarat@gmail.com>
2021-03-08 12:59:09 +0200

    use functions

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
 var animalName = prompt("What is your favourite animal Cat or Dog ?");
 
-while(!(animalName.toLowerCase()=="cat" || animalName.toLowerCase()=="dog"))
+function writeAnimal(animal)
 {
-    animalName = prompt("What is your favourite animal Cat or Dog ?");
+while(!(animal.toLowerCase()=="cat" || animal.toLowerCase()=="dog"))
+{
+    animal = prompt("What is your favourite animal Cat or Dog ?");
+}
+return animal;
 }
 
-if(animalName.toLowerCase() == "cat")
+function checkAnimal(animal)
+{
+if(animal.toLowerCase() == "cat")
 {
     var image = "<img src='images/cat.jpg'>";
-    var image1 = "<img src='images/cat1.jpg'>";
+    
 }
 else
 {
     var image = "<img src='images/dog.jpg'>";
-    var image1 = "<img src='images/dog1.jpg'>";
+    
+}
+return image;
 }
 
-document.write(image1+'<br>');
-
+function repeatImg(image)
+{
 var counter = 0;
 var imageSum = "";
 counter = prompt("How many times you want to show the animal image ?");
 
-while(counter > 1 )
+while(counter >= 1 )
 {
     imageSum = imageSum + image;
     counter -- ;
 }
+return imageSum;
+}
 
-document.write(imageSum);
+var animalName = writeAnimal(animalName);
+
+var imageName  = checkAnimal(animalName);
+
+var imageNumber = repeatImg(imageName);
+
+document.write(imageNumber);
